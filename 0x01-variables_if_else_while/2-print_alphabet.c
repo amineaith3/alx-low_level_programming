c1533c5cebe66de1a52d7ada4168cce77099f9ca
Amine <amineaithamma2004@gmail.com>
2023-07-14 00:54:52 -0700

    Commited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Print the alphabet using putchar
+    char c;
+    for (c = 'a'; c <= 'z'; c++) {
+        putchar(c);
+    }
+
+    // Print a new line using putchar
+    putchar('\n');
+
+    return 0;
+}
+
