572e35e87a8a0e0149f54cc68105591f4d2c46dd
Amine <amineaithamma2004@gmail.com>
2023-07-20 18:39:33 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,17 @@
 int main(void)
 {
 		int i;
-		for (i = 1; i <= 100; i++){
-			if ((i % 3 == 0) && ( i % 5 != 0)){
+		for (i = 1; i <= 100; i++)
+		{	if ((i % 3 == 0) && ( i % 5 != 0))
 				printf("Fizz");
-			} else if ((i % 5 == 0) && ( i % 3 != 0)){
+			else if ((i % 5 == 0) && ( i % 3 != 0))
 				printf("Buzz");
-			} else if ((i % 3 == 0) && ( i % 5 == 0)){
+			else if ((i % 3 == 0) && ( i % 5 == 0))
 				printf("FizzBuzz");
-			} else {
+			else 
 				printf("%d", i);
-			}
-			if (i!=100){
+			if (i!=100)
 				printf(" ");
-			}
 		}
 		printf("\n");
 		return (0);
