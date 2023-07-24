ec01a335c7d103d1a8de5dfa73749ef9159aaddc
Amine <amineaithamma2004@gmail.com>
2023-07-24 02:55:19 -0700

    Pointers_Arrays_Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,16 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (s[count])
+	while (count => 0)
+	{
+		if (s[count] == '\0)
+		{
+			break;
+		}
 		count++;
+	}
 
-	for (count; count >= 0; count--)
+	for (count--; count >= 0; count--)
 		_putchar(s[count]);
 	_putchar('\n');
 }
