15d87487d7b1e48d0672db1f2dcbc06de24c797f
Amine <amineaithamma2004@gmail.com>
2023-08-04 17:45:11 -0700

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 
 /**
  * main - print number of arguments passed
- * @argc : number of commandline args
- * @argv : pointer to an array of command line args
+ * @argc: number of commandline args
+ * @argv: pointer to an array of command line args
  * Return: 0-success, non-zero is fail.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
         printf("%d\n", argc - 1);
         return (0);
