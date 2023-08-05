7f902f92573b4951f1cf3199779bb8b3644a394e
Amine <amineaithamma2004@gmail.com>
2023-08-04 17:51:38 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	while (--argc)
 	{
-		for (c = argv[argc]; *c, c++)
+		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
