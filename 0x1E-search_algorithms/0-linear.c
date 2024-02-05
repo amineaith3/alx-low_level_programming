5f2f5811e13c67092f5a1974a6cc5772b32bd19a
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-02-05 21:53:21 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,29 +1,24 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
 
-/*
+/**
  * linerar_search - function that searches for an element in an array
- * @array - argument of type pointer int
- * @size - argument of type size_t
- * @value - argument of type int
- * Return - the value if found, otherwise -1, null if array is  null
+ * @array: argument of type pointer int
+ * @size: argument of type size_t
+ * @value: argument of type int
+ * Return: the value if found, otherwise -1, null if array is  null
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-      int i = 0;
-      while(i < size && array)
-      {
-	      printf("Value checked array[%d] = [%d]", i, *array);
-	      if(*array == value)
-	      {
-		      printf("Found %d at index: %d", value, i);
-		      return i;
-		      }
-	      i++;
-	      array++;
-      }
-           if(!array)
-  { 
-    return -1;
-  }
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
