7ac481d7441cc8f0cdb4d4075b0ff45308d0016c
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-02-05 20:45:37 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,18 @@
-/*
+#include<stdio.h>
+
+int linear_search(int *array, size_t size, int value){
+  int i = 0;
+  while(i < size && array){
+  printf("Value checked array[%d] = [%d]", i, *array);
+    if( *array == value){
+      printf("Found %d at index: %d", value, i);
+      return i;
+    }
+    i++;
+    array++;
+  }
+  if(!array)
+  { 
+    return -1;
+  }
+}
